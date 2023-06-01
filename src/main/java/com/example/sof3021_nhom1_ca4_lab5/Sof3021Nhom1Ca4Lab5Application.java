e63a8bd138dd93fcedcae25941a75c7bdfbd1d95
Totti <tandunglecong@gmail.com>
2023-06-01 10:48:05 +0700

    first commit

---
+++ src/main/java/com/example/sof3021_nhom1_ca4_lab5/Sof3021Nhom1Ca4Lab5Application.java
@@ -0,0 +1,13 @@
+package com.example.sof3021_nhom1_ca4_lab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sof3021Nhom1Ca4Lab5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sof3021Nhom1Ca4Lab5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sof3021_nhom1_ca4_lab5/Sof3021Nhom1Ca4Lab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sof3021_nhom1_ca4_lab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sof3021Nhom1Ca4Lab5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
